d6dc2795d9c9a9b6217135d0c6fdf0f1b22e891a
udaygaidhane30 <udaygaidhane30@outlook.com>
2024-10-20 17:30:53 +0530

    day1 mid

---
+++ 02_comments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "testing comments"
+
+#One line comment
+
+<<comment
+Testing
+multiline
+comments
+comment
---
+++ 03_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name="Uday"
+
+surname="Gaidhane"
+
+fullName="$name Gaidhane"
+echo $fullName
+
+full_name=$name" "$surname
+
+echo $full_name
